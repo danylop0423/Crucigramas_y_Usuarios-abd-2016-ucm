5f603bfc1ae0a28b4ffcec97702d28ed6d694b3a
David Labrador <davlabrador@gmail.com>
2016-03-31 22:22:24 +0200

    Added delivery second model

---
+++ src/main/java/abd/p1/model/Msj_invitacion.java
@@ -0,0 +1,43 @@
+package p1admin.model;
+
+import java.sql.Timestamp;
+
+public class Msj_invitacion extends Mensaje {
+	private String xdefecto;
+	private Pregunta pregunta;
+	
+	public Msj_invitacion(Integer id, Usuario emisor, Usuario receptor, Timestamp fecha, boolean leido) {
+		super(id, emisor, receptor, fecha, leido);
+		
+	}
+
+	/**
+	 * @return the xdefecto
+	 */
+	public String getXdefecto() {
+		return xdefecto;
+	}
+
+	/**
+	 * @param xdefecto the xdefecto to set
+	 */
+	public void setXdefecto(String xdefecto) {
+		this.xdefecto = xdefecto;
+	}
+
+	/**
+	 * @return the pregunta
+	 */
+	public Pregunta getPregunta() {
+		return pregunta;
+	}
+
+	/**
+	 * @param pregunta the pregunta to set
+	 */
+	public void setPregunta(Pregunta pregunta) {
+		this.pregunta = pregunta;
+	}
+
+	
+}
